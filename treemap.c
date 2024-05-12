ad9d9c96996da103b6f4a8ccdde14b571ec98b12
Your Name <you@example.com>
2024-05-12 05:28:54 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -185,11 +185,12 @@ Pair * upperBound(TreeMap * tree, void* key) {
         if (tree->lower_than(key, aux->pair->key)){
             ub = aux;
             aux = aux->left;
-        }/*
+        }
+        /*
         else{
             aux = aux->right;
         }
-    */
+        */
     }
 }
 
